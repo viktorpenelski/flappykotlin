7c860bae3fb90ba90085a55e5a88bb8f175d4902
byfo <vic.nqq@gmail.com>
2017-10-24 01:02:13 +0300

    Score is now displayed durign gameplay

--- core/src/com/flappy/kotlin/game/states/PlayState.kt
+++ core/src/com/flappy/kotlin/game/states/PlayState.kt
@@ -2,14 +2,16 @@ package com.flappy.kotlin.game.states
 
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.Color
+import com.badlogic.gdx.graphics.OrthographicCamera
 import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.g2d.BitmapFont
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer
 import com.badlogic.gdx.math.Vector2
 import com.badlogic.gdx.utils.Array
 import com.flappy.kotlin.game.FlappyKotlinGame
 import com.flappy.kotlin.game.sprites.Player
 import com.flappy.kotlin.game.sprites.Tube
+
 
 /**
  * Created by Vic on 9/27/2017.
@@ -28,8 +30,8 @@ class PlayState(gameStateManager: GameStateManager) : State(gameStateManager) {
     private val ground = Texture("ground.png")
     private val groundPos1 = Vector2((camera.position.x - camera.viewportWidth / 2), GROUND_Y_OFFSET)
     private val groundPos2 = Vector2((camera.position.x - camera.viewportWidth / 2) + ground.width, GROUND_Y_OFFSET)
-    private val shapeRenderer = ShapeRenderer()
     private var score = 0
+    private var font = BitmapFont()
 
     init {
         camera.setToOrtho(FlappyKotlinGame.WIDTH / 2, FlappyKotlinGame.HEIGHT / 2)
@@ -49,19 +51,50 @@ class PlayState(gameStateManager: GameStateManager) : State(gameStateManager) {
         updateGround()
         player.update(dt)
         camera.position.x = player.position.x + CAM_OFFSET
-
-        for (tube in tubes) {
-            repositionOffScreenTube(tube)
-            if (checkForCollision(tube)) {
-                stateManager.set(MenuState(stateManager))
-                break
-            }
-        }
+        updateTubes()
 
         if (player.position.y <= ground.height + GROUND_Y_OFFSET) {
             stateManager.set(MenuState(stateManager))
         }
         camera.update()
+    }
+
+    private fun updateTubes() {
+        for (tube in tubes) {
+            repositionOffScreenTube(tube)
+            scorePoint(tube)
+            if (tube.collides(player.bounds)) {
+                stateManager.set(MenuState(stateManager))
+                break
+            }
+        }
+    }
+
+    override fun render(sb: SpriteBatch) {
+        sb.projectionMatrix = camera.combined
+        sb.begin()
+        sb.draw(background, camera.position.x - (camera.viewportWidth / 2), 0F)
+        sb.draw(player.getTexture(), player.position.x, player.position.y)
+        sb.renderTubes()
+        sb.draw(ground, groundPos1.x, groundPos1.y)
+        sb.draw(ground, groundPos2.x, groundPos2.y)
+        sb.renderScore()
+        sb.end()
+    }
+
+    private fun SpriteBatch.renderTubes() {
+        for (tube in tubes) {
+            this.draw(tube.topTube, tube.posTopTube.x, tube.posTopTube.y)
+            this.draw(tube.bottomTube, tube.posBotTube.x, tube.posBotTube.y)
+        }
+    }
+
+    private fun SpriteBatch.renderScore() {
+        val relativeVector = relativeVector(5F, 15F, camera)
+        font.region.texture.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear)
+        font.color = Color.WHITE
+        font.setUseIntegerPositions(false)
+        font.draw(this, "Score: $score", relativeVector.x, relativeVector.y)
     }
 
     private fun repositionOffScreenTube(tube: Tube) {
@@ -70,52 +103,19 @@ class PlayState(gameStateManager: GameStateManager) : State(gameStateManager) {
         }
     }
 
-    private fun checkForCollision(tube: Tube) = tube.collides(player.bounds)
-
-    override fun render(sb: SpriteBatch) {
-        sb.projectionMatrix = camera.combined
-        sb.begin()
-        initializeDebugRenderer()
-        sb.draw(background, camera.position.x - (camera.viewportWidth / 2), 0F)
-        sb.draw(player.getTexture(), player.position.x, player.position.y)
-        for (tube in tubes) {
-            sb.draw(tube.topTube, tube.posTopTube.x, tube.posTopTube.y)
-            sb.draw(tube.bottomTube, tube.posBotTube.x, tube.posBotTube.y)
-            drawDebugTubeScoreLine(tube)
-            scorePoint(tube)
-        }
-
-        sb.draw(ground, groundPos1.x, groundPos1.y)
-        sb.draw(ground, groundPos2.x, groundPos2.y)
-        shapeRenderer.end()
-        sb.end()
-    }
-
     private fun scorePoint(tube: Tube) {
-        if (player.position.x >= calculateStartOfScoreLine(tube).x && !tube.scored) {
+        if (player.position.x >= (tube.posTopTube.x + tube.topTube.width) && !tube.scored) {
             score++
             tube.scored = true
             LOG.info("Score: $score")
         }
     }
 
-    private fun initializeDebugRenderer() {
-        shapeRenderer.begin(ShapeRenderer.ShapeType.Line)
-        shapeRenderer.projectionMatrix = camera.combined
-        shapeRenderer.color = Color.RED
-    }
-
-    private fun drawDebugTubeScoreLine(tube: Tube) {
-        shapeRenderer.line(calculateStartOfScoreLine(tube), calculateEndOfScoreLine(tube))
-    }
-
-    private fun calculateStartOfScoreLine(tube: Tube) = Vector2(tube.posTopTube.x + tube.topTube.width.toFloat(), tube.posTopTube.y)
-    private fun calculateEndOfScoreLine(tube: Tube) = Vector2(tube.posBotTube.x + tube.bottomTube.width, tube.posBotTube.y + tube.bottomTube.height)
-
     override fun dispose() {
         background.dispose()
         player.dispose()
         ground.dispose()
+        font.dispose()
         for (tube in tubes) {
             tube.dispose()
         }
@@ -130,4 +130,7 @@ class PlayState(gameStateManager: GameStateManager) : State(gameStateManager) {
             groundPos2.add((ground.width * 2).toFloat(), 0F)
         }
     }
+
+    private fun relativeVector(x: Float, y: Float, c: OrthographicCamera) =
+            Vector2(x + c.position.x - c.viewportWidth / 2, y + c.position.y - c.viewportHeight / 2)
 }
